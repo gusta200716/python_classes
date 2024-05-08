cc14a7f820db9a3ab47cfc13561d9c1a6c6a4d46
gusta200716 <130071855+gusta200716@users.noreply.github.com>
2024-05-08 08:12:21 -0300

    Add files via upload

--- gustavo.py
+++ gustavo.py
@@ -1,3 +1,4 @@
+
 class Cliente: 
     def __init__ (self,nome,anoNascimento,sexo,saldo,endereco,ativo):
         self.nome = nome
@@ -21,16 +22,43 @@ class Cliente:
             print("O cliente", self.nome, "está ativo.")
         else:
             print("O cliente", self.nome, "não está ativo.")
+    
+    def calcularIdade(self):
+        anoAtual = 2024
+        idade = anoAtual - self.anoNascimento
+        print("A idade do cliente é:", idade, "anos")
+        
+    def VerificaSaldo(self):
+        if self.saldo >=0:
+            print("O saldo", self.saldo, "está positivo")
+        else:
+            print("O saldo", self.saldo, "está negativo")
+
         
         
-objeto = Cliente("Gustavo", 2007, "M", 500, "Rua São Paulo", True)
-objeto2 = Cliente("Gustavo", 2007, "M", -500, "Rua São Paulo", True)
-objeto3 = Cliente("Gustavo", 2007, "M", 500, "Rua São Paulo", False)
+objeto = Cliente("Gustavo", 2007, "M", 245, "Rua São Paulo", True)
+objeto2 = Cliente("Jose", 2003, "M", -789, "Rua São Paulo", True)
+objeto3 = Cliente("Rodolfo", 2001, "M", 457, "Rua São Paulo", False)
 # objeto.imprimir()
 # objeto2.imprimir()
 # objeto3.imprimir()
 
-objeto.ValidaCliente()
-objeto2.ValidaCliente()
-objeto3.ValidaCliente()
+# objeto.ValidaCliente()
+# objeto2.ValidaCliente()
+# objeto3.ValidaCliente()
+
+print("---------")
+#objeto.calcularIdade()
+#objeto2.calcularIdade()
+#objeto3.calcularIdade()
+
+objeto.VerificaSaldo()
+objeto2.VerificaSaldo()
+objeto3.VerificaSaldo()
+
+
+
+
+
+    
 
