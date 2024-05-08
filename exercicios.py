ca3ed678d8c1e93014b5a722dad515fe3feb1393
gusta200716 <130071855+gusta200716@users.noreply.github.com>
2024-05-08 08:37:57 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,28 @@
+class Exercicios:
+    def __init__ (self,nomeAluno, numeroChamada):
+        self.nomeAluno = nomeAluno
+        self.numeroChamada = numeroChamada
+        
+    def Exercicio1(self):
+        nome = "Gustavo"
+        idade = "16"
+        sexo = "M"
+        status = "ativo"
+        print("Nome:",nome)
+        print("Idade:",idade)
+        print("Sexo:",sexo)
+        print("Status:",status)
+    
+     def Exercicio2(self):
+        nome = "Maria"
+        idade = 25
+        sexo = "Feminino"
+        status = "Estudante"
+        
+    
+    # def Exercicio3(self):
+    
+    
+ex = Exercicios("gustavo", 7)
+ex.Exercicio2()
+        
