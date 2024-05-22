691254be968b18dd84db1285d3b1b053147bdf1c
gusta200716 <130071855+gusta200716@users.noreply.github.com>
2024-05-22 08:02:36 -0300

    Add files via upload
    aula hj

---
+++ class exercicio:.py
@@ -0,0 +1,12 @@
+class exercicio:
+    def __init__(self):
+        print('-')
+
+    def exercicio1(self,nome,idade,salario,cargo,turno,setor):
+        print("Meu nome é:", nome, "Tenho:", idade, "Meu salário é:", salario, "Meu cargo é:", cargo, "Meu turno é:", turno, "Meu setor é:",setor)
+
+    def exercicio2(self,NomeEscola,estado,NumeroProfessores,cidade,NumeroMilitares,logradouro,NumeroEndereco,NumeroAlunos,ColegioMilitar)
+
+
+ex = exercicio()
+ex.exercicio1(nome = "Gu")
